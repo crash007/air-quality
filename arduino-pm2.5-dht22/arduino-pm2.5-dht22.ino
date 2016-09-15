318527d7a54219204736d6b790f7aae273b81d2a
Petter Wallin <wallin.petter@gmail.com>
2016-09-15 23:01:40 +0200

    samplar mätvärden fler gånger på partikelsensorn

--- arduino-pm2.5-dht22/arduino-pm2.5-dht22.ino
+++ arduino-pm2.5-dht22/arduino-pm2.5-dht22.ino
@@ -1,9 +1,13 @@
+#include <RunningMedian.h>
+
+
 #include "DHT.h"
 #define DHTPIN 2     // what digital pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 
-DHT dht(DHTPIN, DHTTYPE);
 
+DHT dht(DHTPIN, DHTTYPE);
+RunningMedian measurements = RunningMedian(20);
 int measurePin = 6;
 int ledPower = 12;
  
@@ -20,39 +24,42 @@ void setup(){
 }
  
 void loop(){
-  digitalWrite(ledPower,LOW); // power on the LED
-  delayMicroseconds(samplingTime);
- 
-  voMeasured = analogRead(measurePin); // read the dust value
- 
-  delayMicroseconds(deltaTime);
-  digitalWrite(ledPower,HIGH); // turn the LED off
-  delayMicroseconds(sleepTime);
- 
-  // 0 - 5V mapped to 0 - 1023 integer values
-  // recover voltage
-  //calcVoltage = voMeasured * (5.0 / 1024);
- 
-  // linear eqaution taken from http://www.howmuchsnow.com/arduino/airquality/
-  // Chris Nafis (c) 2012
-  //dustDensity = (0.17 * calcVoltage - 0.12)*1000;
- 
-  //Serial.print("Raw Signal Value (0-1023): ");
-  //Serial.print(voMeasured);
+  long start = millis();
+  
+  //Varje mätning tar 10ms. samplar 100 gånger under 1 sekund
+  for(int x=0; x<20; x++){
+    digitalWrite(ledPower,LOW); // power on the LED
+    delayMicroseconds(samplingTime);
+   
+    //voMeasured = analogRead(measurePin); // read the dust value
+    measurements.add(analogRead(measurePin));
+      
+    delayMicroseconds(deltaTime);
+    digitalWrite(ledPower,HIGH); // turn the LED off
+    delayMicroseconds(sleepTime); //10ms totalt
+    delay(90); //100ms
+  }//2sekunder
  
   Serial.print("PM2.5:");
-  Serial.println(voMeasured);
+  Serial.println(measurements.getMedian());
+  Serial.println(measurements.getLowest());
+  Serial.println(measurements.getHighest());
  
   //Serial.print(" Density:");
   //Serial.print(dustDensity);
-  
+  delay(1000); //3sek
   float h = dht.readHumidity();
    float t = dht.readTemperature();
+  
     Serial.print("Humidity:");
   Serial.println(h);
+  delay(100);
   Serial.print("Temperature:");
   Serial.println(t);
    
- 
-  delay(5000);
+ long time = millis()-start;
+ Serial.print("Iteration time:");
+ Serial.println(time);
+  //delay(1900); //5sek totalt
+  delay(60000-time); //55 sek
 }
